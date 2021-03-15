fde197c6446b40706e71a800f3295684c2b2781e
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-03-15 19:07:01 +0100

    Login and Logout functions initialized

--- Webshop/Helpers/WebbShopAPI.cs
+++ Webshop/Helpers/WebbShopAPI.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace WebbShopApi.Helpers
 {
-    public class WebbShopAPI
+    public static class WebbShopAPI
     {
+        public static int Login(string username, string password)
+        {
+            return 0;
+        }
+
+        public static void Logout(string username, string password)
+        {
+        }
     }
 }
