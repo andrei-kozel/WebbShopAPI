1bed38bf9ddde4408bd5b1a6f0f59bd6100b6237
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-03-15 17:55:36 +0100

    DB initial setup

--- Webshop/Database/MyContext.cs
+++ Webshop/Database/MyContext.cs
@@ -4,9 +4,15 @@ using System.Text;
 
 namespace Webshop.Database
 {
-    class MyContext
+using Microsoft.EntityFrameworkCore;
+
+class MyContext : DbContext
     {
         private const string DatabaseName = "WebbShopAndreiKozel";
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer($@"Server = .\SQLEXPRESS;Database={DatabaseName};trusted_connection=true");
+        }
     }
 }
