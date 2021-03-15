e2a40c5bf29ce52c60cab41ec3370472bb095210
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-03-15 18:10:27 +0100

    name changed

--- Webshop/Models/Book.cs
+++ Webshop/Models/Book.cs
@@ -4,14 +4,13 @@ using System.Text;
 
 namespace WebbShopApi.Models
 {
-    class SoldBooks
+    class Book
     {
         public int ID { get; set; }
-        public string Title { get; set; }
+        public string Name { get; set; }
         public string Author { get; set; }
-        public int CategoryId { get; set; }
         public int Price { get; set; }
-        public DateTime PurchaseDate { get; set; }
-        public int UserId { get; set; }
+        public int Amount { get; set; }
+        public int CategoryId { get; set; }
     }
 }
--- Webshop/Models/SoldBook.cs
+++ Webshop/Models/SoldBook.cs
@@ -4,13 +4,14 @@ using System.Text;
 
 namespace WebbShopApi.Models
 {
-    class Books
+    class SoldBook
     {
         public int ID { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string Author { get; set; }
-        public int Price { get; set; }
-        public int Amount { get; set; }
         public int CategoryId { get; set; }
+        public int Price { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public int UserId { get; set; }
     }
 }
