a8d9ba24d5122aef59fce4a535e93b2da90a1fe7
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-03-15 18:01:18 +0100

    Books initial commit

---
+++ Webshop/Models/Books.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebbShopApi.Models
+{
+    class Books
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Price { get; set; }
+        public int Amount { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
