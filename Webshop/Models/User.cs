ed6aa53f4ba6bad85f2695172663505d7cba8ce4
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-03-15 17:55:08 +0100

    User model initial commit

---
+++ Webshop/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Webshop.Models
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; } = "Codic2021";
+        public DateTime LastLogin { get; set; }
+        public DateTime SessionTimer { get; set; }
+        public bool IsActiove { get; set; } = true;
+        public bool IsAdmin { get; set; } = false;
+    }
+}
