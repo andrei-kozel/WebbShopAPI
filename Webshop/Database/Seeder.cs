c2fee8ec3939792def418cac146409ee80f28d13
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-03-15 21:04:47 +0100

    Seeder class for generate basedata

---
+++ Webshop/Database/Seeder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebbShopApi.Models;
+
+namespace WebbShopApi.Database
+{
+    public static class Seeder
+    {
+        public static void Seed()
+        {
+            using (var db = new MyContext())
+            {
+                if (db.BookCategories.Count() == 0)
+                {
+                    db.BookCategories.Add(new BookCategory { Name = "Horror" });
+                    db.BookCategories.Add(new BookCategory { Name = "Humor" });
+                    db.BookCategories.Add(new BookCategory { Name = "Science Fiction" });
+                    db.BookCategories.Add(new BookCategory { Name = "Romance" });
+                    db.SaveChanges();
+                }
+
+                if (db.Users.Count() == 0)
+                {
+                    db.Users.Add(new User { Name = "CodicRulez", IsAdmin = true });
+                    db.Users.Add(new User { Name = "Codic2021", IsAdmin = false });
+                    db.SaveChanges();
+
+                }
+
+                if (db.Books.Count() == 0)
+                {
+                    db.Books.Add(new Book { Name = "Cabal (Nightbreed)", Author = "Clive Barker", Price = 250, Amount = 3, CategoryId = 1 });
+                    db.Books.Add(new Book { Name = "The Shining", Author = "Stephen King", Price = 200, Amount = 2, CategoryId = 1 });
+                    db.Books.Add(new Book { Name = "Doctor Sleep", Author = "Stephen King", Price = 200, Amount = 1, CategoryId = 1 });
+                    db.Books.Add(new Book { Name = "I Robot", Author = "Isaac Asimov", Price = 150, Amount = 4, CategoryId = 3 });
+                    db.SaveChanges();
+
+                }
+            }
+        }
+    }
+}
