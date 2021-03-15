6db377ea3876664faf6a5d40ff6d53992e79c3e7
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-03-15 19:45:36 +0100

    G methods generated

--- Webshop/Helpers/WebbShopAPI.cs
+++ Webshop/Helpers/WebbShopAPI.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WebbShopApi.Models;
 
 namespace WebbShopApi.Helpers
 {
@@ -11,8 +12,108 @@ namespace WebbShopApi.Helpers
             return 0;
         }
 
-        public static void Logout(string username, string password)
+        public static void Logout(int userId)
         {
+        }
+
+        public static List<BookCategory> GetCategories()
+        {
+            return new List<BookCategory>();
+        }
+
+        public static List<BookCategory> GetCategories(string keyword)
+        {
+            return new List<BookCategory>();
+        }
+
+        public static string GetCategory(int categoryId)
+        {
+            return String.Empty;
+        }
+
+        public static string GetBook(int bookId)
+        {
+            return String.Empty;
+        }
+
+        public static List<Book> GetBooks(string keyword)
+        {
+            return new List<Book>();
+        }
+
+        public static List<Books> GetAuthors(string keyword)
+        {
+            return new List<Books>();
+        }
+
+        public static bool BuyBook(int userId, int bookId)
+        {
+            return false;
+        }
+
+        public static string Ping(int userId) {
+            return "Pong";
+        }
+
+        public static bool Register(string name, string password, string passwordVerify)
+        {
+            return false;
+        }
+
+        // ADMIN FUNCTIONS
+        public static bool AddBook(int adminId, int Id, string title, string author, int price, int amount )
+        {
+            return false;
+        }
+
+        public static void SetAmount(int adminId, int bookId)
+        {
+
+        }
+
+        public static List<String> ListUsers(int adminId)
+        {
+            return new List<String>();
+        }
+
+        public static List<String> FindUser(int adminId, string keyword)
+        {
+            return new List<String>();
+        }
+
+        public static bool UpdateBook(int adminId, int Id, string title, string author, int price)
+        {
+            return false;
+        }
+
+        public static bool DeleteBook(int adminId, int bookId)
+        {
+            return false;
+        }
+
+        public static bool AddCategory(int adminId, string name)
+        {
+            return false;
+        }
+
+        public static bool AddBookToCategory(int adminId, int bookId, int categoryId)
+        {
+            return false;
+        }
+
+        public static bool UpdateCategory(int adminId, int categoryId, string name)
+        {
+            return false;
+        }
+
+        public static bool DeleteCategory(int adminId, int categoryId)
+        {
+            return false;
+        }
+
+        public static bool AddUser(int adminId, string name, string password)
+        {
+            return false;
         }
     }
 }
