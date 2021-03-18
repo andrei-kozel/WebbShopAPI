50c9f4e9f8c0d1f967242cdbb4c52deb5a3b29a5
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-03-18 20:27:35 +0100

    examples methods

--- Webshop/Program.cs
+++ Webshop/Program.cs
@@ -7,28 +7,20 @@ namespace WebbShopApi
 {
     class Program
     {
+        /// <summary>
+        /// Mian method
+        /// </summary>
         static void Main(string[] args)
         {
             Seeder.Seed();
-            //Example1();
-            //Example2();
-            //Example3();
+            Example1();
+            Example2();
+            Example3();
         }
 
-        private static void Example3()
-        {
-            LoginAsAdmin();
-            WebbShopAPI.AddUser(2, "Test", "Test");
-        }
-
-        private static void Example2()
-        {
-            LoginAsAdmin();
-            CreateCategory();
-            MoveBookToNewCategory();
-        }
-
-     
+        /// <summary>
+        /// example 1
+        /// </summary>
         private static void Example1()
         {
             LogInAsTestUser();
@@ -41,6 +33,28 @@ namespace WebbShopApi
             GetAmount(1);
         }
 
+        /// <summary>
+        /// example 2
+        /// </summary>
+        private static void Example2()
+        {
+            LoginAsAdmin();
+            CreateCategory();
+            MoveBookToNewCategory();
+        }
+
+        /// <summary>
+        /// example 3
+        /// </summary>
+        private static void Example3()
+        {
+            LoginAsAdmin();
+            WebbShopAPI.AddUser(2, "Test", "Test");
+        }
+
+        /// <summary>
+        /// move book with id 1 to category with id 3
+        /// </summary>
         private static void MoveBookToNewCategory()
         {
             Console.WriteLine("TEST: move book to new category ");
@@ -56,7 +70,9 @@ namespace WebbShopApi
             Continue();
         }
 
-
+        /// <summary>
+        /// create "Test category"
+        /// </summary>
         private static void CreateCategory()
         {
             Console.WriteLine("TEST: create category ");
@@ -72,6 +88,9 @@ namespace WebbShopApi
             Continue();
         }
 
+        /// <summary>
+        /// Login as admin
+        /// </summary>
         private static void LoginAsAdmin()
         {
             Console.WriteLine("TEST: Login as an admin ");
@@ -87,6 +106,10 @@ namespace WebbShopApi
             Continue();
         }
 
+        /// <summary>
+        /// Buy a book
+        /// </summary>
+        /// <param name="bookId"></param>
         private static void BuyBook(int bookId)
         {
             Console.WriteLine("TEST: Buy book");
@@ -103,6 +126,10 @@ namespace WebbShopApi
             Continue();
         }
 
+        /// <summary>
+        /// Get amount 
+        /// </summary>
+        /// <param name="id"></param>
         private static void GetAmount(int id)
         {
             Console.WriteLine($"TEST: Get amount by id {id}");
@@ -112,6 +139,10 @@ namespace WebbShopApi
             Continue();
         }
 
+        /// <summary>
+        /// Get book info with keyword
+        /// </summary>
+        /// <param name="v"></param>
         private static void GetBook(string v)
         {
             Console.WriteLine($"TEST: List books contains {v}");
@@ -123,7 +154,9 @@ namespace WebbShopApi
             }
             Continue();
         }
-
+        /// <summary>
+        /// get horrors book
+        /// </summary>
         private static void GetHorrorsBooks()
         {
             Console.WriteLine("TEST: List horror books");
@@ -136,6 +169,9 @@ namespace WebbShopApi
             Continue();
         }
 
+        /// <summary>
+        /// get categories
+        /// </summary>
         private static void GetCategories()
         {
             Console.WriteLine("TEST: List all categories");
@@ -148,6 +184,9 @@ namespace WebbShopApi
             Continue();
         }
 
+        /// <summary>
+        /// ask user to press enter to continue
+        /// </summary>
         private static void Continue()
         {
             Console.WriteLine();
@@ -156,6 +195,9 @@ namespace WebbShopApi
             Console.Clear();
         }
 
+        /// <summary>
+        /// login as a test user
+        /// </summary>
         private static void LogInAsTestUser()
         {
             Console.WriteLine("TEST: Login as a test user Codic2021");
